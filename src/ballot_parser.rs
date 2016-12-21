a67e0d14f3c23de494a2005104b30f9d7307c14f
Anders Kaseorg <andersk@mit.edu>
2016-12-20 19:39:00 -0500

    Friendlier error handling
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

---
+++ src/ballot_parser.rs
@@ -0,0 +1,100 @@
+use gmp::mpq::Mpq;
+use gmp::mpz::Mpz;
+use std::borrow::Borrow;
+use std::collections::hash_map::{Entry, HashMap};
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read, stdin};
+use std::result::Result;
+use std::str::FromStr;
+
+pub struct BallotParser {
+    pub candidates: Vec<String>,
+    pub candidate_index: HashMap<String, usize>,
+    pub ballots: Vec<(Box<[Box<[usize]>]>, Mpq)>,
+}
+
+impl BallotParser {
+    fn new() -> BallotParser {
+        return BallotParser {
+            candidates: Vec::new(),
+            candidate_index: HashMap::new(),
+            ballots: Vec::new(),
+        };
+    }
+
+    fn parse_candidate(&mut self, name: &str) -> usize {
+        let name = name.trim();
+        match self.candidate_index.entry(name.to_string()) {
+            Entry::Occupied(e) => *e.get(),
+            Entry::Vacant(e) => {
+                let n = self.candidates.len();
+                e.insert(n);
+                self.candidates.push(name.to_string());
+                n
+            }
+        }
+    }
+
+    fn parse_group(&mut self, group: &str) -> Box<[usize]> {
+        group.split('=')
+            .map(|name| self.parse_candidate(name))
+            .collect::<Vec<_>>()
+            .into_boxed_slice()
+    }
+
+    fn parse_groups(&mut self, groups: &str) -> Box<[Box<[usize]>]> {
+        groups.split('>')
+            .map(|group| self.parse_group(group))
+            .collect::<Vec<_>>()
+            .into_boxed_slice()
+    }
+
+    fn add_ballot(&mut self, line: &str) -> Result<(), String> {
+        let line = line.trim();
+        if line.is_empty() {
+            return Ok(());
+        }
+
+        let (w, groups) = match line.find(':') {
+            Some(i) => {
+                (Mpq::ratio(&Mpz::from_str(&line[..i]).map_err(|()|
+                                                               "cannot parse ballot weight"
+                )?, &Mpz::one()), &line[i + 1..])
+            }
+            None => (Mpq::one(), &line[..]),
+        };
+
+        let ballot = (self.parse_groups(groups), w);
+        self.ballots.push(ballot);
+        Ok(())
+    }
+
+    fn add_ballots<R: Read>(&mut self, buf: BufReader<R>) -> Result<(), (usize, String)> {
+        for (lineno, line) in buf.lines().enumerate() {
+            self.add_ballot(&line.map_err(|e| (lineno, String::from(e.description())))?)
+                .map_err(|e| (lineno, e))?;
+        }
+        Ok(())
+    }
+
+    fn add_ballot_file(&mut self, filename: &str) -> Result<(), String> {
+        let file: Box<Read> = if filename == "-" {
+            Box::new(stdin())
+        } else {
+            Box::new(File::open(filename).map_err(|e| format!("error: {}: {}", filename, e))?)
+        };
+        self.add_ballots(BufReader::new(file))
+            .map_err(|(lineno, e)| format!("{}:{}: error: {}", filename, lineno + 1, e))
+    }
+}
+
+pub fn parse_ballot_files<Str>(filenames: &[Str]) -> Result<BallotParser, String>
+    where Str: Borrow<str>
+{
+    let mut bp = BallotParser::new();
+    for filename in filenames.iter() {
+        bp.add_ballot_file(filename.borrow())?;
+    }
+    Ok(bp)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,19 @@ extern crate getopts;
 extern crate gmp;
 extern crate vote;
 
+mod ballot_parser;
+
+use ballot_parser::parse_ballot_files;
 use getopts::Options;
 use gmp::mpq::Mpq;
-use gmp::mpz::Mpz;
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
 use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader, Read, Write, stdin, stderr};
+use std::io::{Write, stderr};
 use std::process::exit;
-use std::str::FromStr;
 use vote::schulze_stv::schulze_stv;
 
 const USAGE: &'static str = include_str!("usage.txt");
 
-fn main() {
+fn main_result() -> Result<(), String> {
     let args = env::args().collect::<Vec<_>>();
     let program = &args[0];
 
@@ -27,116 +25,67 @@ fn main() {
                 "N");
     opts.optflag("", "help", "show this help message and exit");
     opts.optflag("", "version", "show the program version and exit");
-    let matches = match opts.parse(&args[1..]) {
-        Err(fail) => {
-            writeln!(&mut stderr(), "{}: {}", program, fail).unwrap();
-            exit(1)
-        }
-        Ok(matches) => matches,
-    };
+    let matches = opts.parse(&args[1..]).map_err(|e| format!("{}: error: {}", program, e))?;
 
     if matches.opt_present("help") {
         print!("{}", opts.usage(USAGE));
-        return;
+        return Ok(());
     }
 
     if matches.opt_present("version") {
         println!("elect {}", env!("CARGO_PKG_VERSION"));
-        return;
+        return Ok(());
     }
 
     if matches.free.is_empty() {
-        write!(&mut stderr(), "{}", opts.usage(USAGE)).unwrap();
+        write!(&mut stderr(), "{}", opts.usage(USAGE)).expect("failed printing to stderr");
         exit(1)
     }
 
-    let num_seats = matches.opt_str("w").map(|s| s.parse().unwrap()).unwrap_or(1);
+    let num_seats = matches.opt_str("w")
+        .map(|s| s.parse().map_err(|e| format!("{}: error: -w argument: {}", program, e)))
+        .unwrap_or(Ok(1))?;
 
-    let mut candidates = Vec::new();
-    let mut candidate_index = HashMap::new();
-
-    let ballots = matches.free
-        .iter()
-        .flat_map(|filename| {
-            let file: Box<Read> = if filename == "-" {
-                Box::new(stdin())
-            } else {
-                Box::new(File::open(filename).unwrap())
-            };
-            let buf = BufReader::new(file);
-            buf.lines()
-        })
-        .map(|line| line.unwrap())
-        .filter(|line| !line.trim().is_empty())
-        .map(|line| {
-            let (w, groups) = match line.find(':') {
-                Some(i) => {
-                    (Mpq::ratio(&Mpz::from_str(&line[..i]).unwrap(), &Mpz::one()), &line[i + 1..])
-                }
-                None => (Mpq::one(), &line[..]),
-            };
-            (groups.split('>')
-                 .map(|group| {
-                    group.split('=')
-                        .map(|name| {
-                            let name = name.trim();
-                            match candidate_index.entry(name.to_string()) {
-                                Entry::Occupied(e) => *e.get(),
-                                Entry::Vacant(e) => {
-                                    let n = candidates.len();
-                                    e.insert(n);
-                                    candidates.push(name.to_string());
-                                    n
-                                }
-                            }
-                        })
-                        .collect::<Vec<_>>()
-                        .into_boxed_slice()
-                })
-                 .collect::<Vec<_>>()
-                 .into_boxed_slice(),
-             w)
-        })
-        .collect::<Vec<_>>()
-        .into_boxed_slice();
-
-    if ballots.is_empty() {
-        writeln!(&mut stderr(), "error: No ballots found").unwrap();
-        exit(1);
+    let bp = parse_ballot_files(&matches.free)?;
+    if bp.ballots.is_empty() {
+        return Err(format!("{}: error: No ballots found", program));
     }
 
     println!("Tallying Schulze STV election.");
     println!("");
 
-    println!("Candidates ({}):", candidates.len());
-    let mut candidates_sorted = candidates.clone();
+    println!("Candidates ({}):", bp.candidates.len());
+    let mut candidates_sorted = bp.candidates.clone();
     candidates_sorted.sort();
     for candidate in &candidates_sorted {
         println!("  {}", candidate);
     }
     println!("");
 
-    let total_weight = ballots.iter().map(|&(_, ref w)| w).fold(Mpq::zero(), |acc, x| acc + x);
+    let total_weight = bp.ballots.iter().map(|&(_, ref w)| w).fold(Mpq::zero(), |acc, x| acc + x);
     println!("Ballots ({:?}):", total_weight);
-    for &(ref groups, ref w) in ballots.iter() {
+    for &(ref groups, ref w) in bp.ballots.iter() {
         println!("  {:?}: {}",
                  w,
                  groups.iter()
                      .map(|group| {
-                         group.iter().map(|&c| &candidates[c][..]).collect::<Vec<_>>().join(" = ")
+                         group.iter()
+                             .map(|&c| &bp.candidates[c][..])
+                             .collect::<Vec<_>>()
+                             .join(" = ")
                      })
                      .collect::<Vec<_>>()
                      .join(" > "));
     }
     println!("");
 
-    let mut winners = schulze_stv(candidates.len(), num_seats, &ballots);
+    let mut winners = schulze_stv(bp.candidates.len(), num_seats, &bp.ballots);
 
     for set in winners.iter_mut() {
-        set.sort_by(|&a, &b| candidates[a].cmp(&candidates[b]));
+        set.sort_by(|&a, &b| bp.candidates[a].cmp(&bp.candidates[b]));
     }
     winners.sort_by(|a, b| {
-        a.iter().map(|&i| &candidates[i]).cmp(b.iter().map(|&i| &candidates[i]))
+        a.iter().map(|&i| &bp.candidates[i]).cmp(b.iter().map(|&i| &bp.candidates[i]))
     });
 
     let set_suffix = if num_seats == 1 { "" } else { " set" };
@@ -147,6 +96,14 @@ fn main() {
     }
     for set in winners.iter() {
         println!("  {}",
-                 set.iter().map(|&c| &candidates[c][..]).collect::<Vec<_>>().join(", "));
+                 set.iter().map(|&c| &bp.candidates[c][..]).collect::<Vec<_>>().join(", "));
     }
+    Ok(())
+}
+
+fn main() {
+    main_result().unwrap_or_else(|e| {
+        writeln!(&mut stderr(), "{}", e).expect("failed printing to stderr");
+        exit(1)
+    })
 }
